92cac6845078e4219e694ebbfa5242d41ed01113
milanh34 <milanh344@gmail.com>
2024-08-29 21:06:25 +0530

    Added authentication and roles in backend

--- backend/controllers/SignIn.js
+++ backend/controllers/SignIn.js
@@ -1,30 +1,68 @@
 import User from "../models/User_schema.js";
 import bcrypt from "bcryptjs";
 
-const signIn = async(req,resp)=>{
-    try
-    {
-        let existingUserEmail = await User.findOne({email:req.body.email});
-        if(!existingUserEmail)
-        {
-            return resp.status(201).json({message:"Invalid email"})
-        }
-        let existingUserPhone = await User.findOne({phone_no:req.body.phone_no});
-        if(!existingUserPhone)
-        {
-            return resp.status(201).json({message:"Invalid phone no"})
-        }
-        const passMatch = await bcrypt.compare(req.body.password, existingUser.password||'');
-        if(!passMatch)
-        {
-            return resp.status(201).json({ message: "Invalid password" });  
-        }
-        return resp.status(201).json({ message: "Sign In succsefully" });      
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    return "error";
+  }
+};
+
+const signIn = async (req, resp) => {
+  try {
+    if (!req.body?.email || !req.body?.password) {
+      return resp.status(400).json({ message: "Email and Password is required" });
     }
-    catch(err)
-    {
-        console.log(err);
-        resp.status(401).json({ message:"Internal server Error" });
+    let existingUser = await User.findOne({ email: req.body.email });
+    if (!existingUser) {
+      return resp.status(400).json({ message: "User does not exist" });
     }
-} 
+    // let existingUserPhone = await User.findOne({phone_no:req.body.phone_no});
+    // if(!existingUserPhone)
+    // {
+    //     return resp.status(201).json({message:"Invalid phone no"})
+    // }
+    const passMatch = await bcrypt.compare(
+      req.body.password,
+      existingUser?.password || ""
+    );
+    if (!passMatch) {
+      return resp.status(400).json({ message: "Invalid password" });
+    }
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+      existingUser._id
+    );
+    const loggedInUser = await User.findById(existingUser._id).select(
+      "-password -refreshToken"
+    );
+    const options = {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'None',
+      maxAge: 365 * 24 * 60 * 60 * 1000,
+    };
+    return resp
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .cookie("role", existingUser?.role, options)
+      .json({
+        user: loggedInUser,
+        accessToken,
+        refreshToken,
+        message: "Sign In succsefully",
+      });
+  } catch (err) {
+    console.log(err);
+    resp.status(500).json({ message: "Internal server Error" });
+  }
+};
 export default signIn;
--- backend/controllers/SignUp.js
+++ backend/controllers/SignUp.js
@@ -1,52 +1,65 @@
 import User from "../models/User_schema.js";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken"
 import sendSMS from "./sendSMS.js";
-
-const secretKey = "123#secret";
 
 const signUp = async (req, resp) => {
   try {
-    // Check if a user with the same first name and last name exists
-    const existingUser = await User.findOne({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName
-    });
+    if (
+      [
+        req.body?.firstName,
+        req.body?.lastName,
+        req.body?.email,
+        req.body?.phone_no,
+        req.body?.date_of_birth,
+        req.body?.password,
+      ].some((field) => typeof field !== 'string' || field.trim() === "")
+    ) {
+      return resp.status(400).json({ message: "All fields are required" });
+    }
 
+    console.log("2");
+    const existingUser = await User.findOne({
+      email: req.body.email,
+    });
     if (existingUser) {
-      return resp.status(201).json({ message: "User already exists! Sign in instead." });
+      return resp
+        .status(409)
+        .json({ message: "User already exists! Sign in instead." });
     }
 
     // Hash the password
     const hashedPass = await bcrypt.hash(req.body.password, 10);
 
     // Create a new user
-    const userdata = new User({
+    const userdata = await User.create({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email,
       phone_no: req.body.phone_no,
       date_of_birth: req.body.date_of_birth,
-      password: hashedPass
+      password: hashedPass,
     });
 
-    // Save the new user
-    let result = await userdata.save();
+    // // Generate a JWT token
+    // const token = jwt.sign({ email: result.email }, secretKey, {
+    //   expiresIn: "1h",
+    // });
 
-    // Generate a JWT token
-    const token = jwt.sign(
-      { email: result.email },
-      secretKey,
-      { expiresIn: '1h' }
-    );
-
-    console.log("Generated Token: ", token);
-    console.log(result);
+    // console.log("Generated Token: ", token);
+    // console.log(result);
 
     // Send SMS
-    await sendSMS(userdata.firstName,userdata.lastName,userdata.phone_no);
+    // await sendSMS(userdata.firstName, userdata.lastName, userdata.phone_no);
 
-    return resp.status(201).json({ token });
+    const createdUser = await User.findById(userdata?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!createdUser) {
+      throw new ApiError(500, "Something went wrong while registering");
+    }
+
+    return resp.status(201).json({ createdUser });
   } catch (err) {
     console.log(err);
     return resp.status(401).json({ message: "Error in creating User" });
