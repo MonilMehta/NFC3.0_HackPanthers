b70ff4d62fd51d64092619eb9d739db0695613ad
MonilMehta <monilmehta5@gmail.com>
2025-01-29 16:16:10 +0530

    Backend env variables set

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import mongoose  from "mongoose";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect("mongodb+srv://milanh344:NFC3_HackPanthers@nfc.u9g1i.mongodb.net/nfc");
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI)
         console.log(`\nMONGODB connected || DB HOST : ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error("MONGODB connection failed: ", error);
